3ed530a268880e7e7ab255b5fd9e09ed9045b260
joohwanhui <jhh9961@gmail.com>
2025-04-07 07:06:28 +0000

    menu

--- task.py
+++ task.py
@@ -18,13 +18,24 @@ n = int(n) #숫자로 변경
 #여기서 부터는 n은 숫자(정수)
 
 #만약에 1을 입력하면
-if n=="1":
+if n== 1:
     print ("멜론 100")
-else:
-    print("1이 아닙니다.")
+#else:
+#   print("1이 아닙니다.")
 #멜론 100 출력
 #만약에 2를 입력하면
+elif n==2 :
+    print("멜론50")
+elif n==3 :
+    print("멜론10")
+elif n==4 : 
+    print("aisong")
+elif n==5 :
+    artist_name=input("가수이름입력:" )
+    print(f"{artist_name} 의 노래")
 #멜론 50 출력
 #...
 #5를 입력하면 가수 이름 검색할 수 있게 입력창이 또 나와야함
 #이름을 입력하면 해당 가수 이름의 노래 리스트가 출력
+else:
+    print("1~5까지 입력하세요")
